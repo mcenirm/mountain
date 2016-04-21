d31c6696849edf06a703cc2f44844d3996df9f2b
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-04-20 19:07:56 -0500

    Basic user authentication preparaton

--- mountain/urls.py
+++ mountain/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]

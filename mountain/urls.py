dcfb2cf479008b6af788c0a9b1d5264560979138
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-04-20 19:48:06 -0500

    Make your API

--- mountain/urls.py
+++ mountain/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
+from .views import ApiEndpoint
+
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
     url(r'^admin/', admin.site.urls),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^api/hello', ApiEndpoint.as_view()),
 ]

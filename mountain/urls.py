fb57ed6650634de14eb799220102ec9f5cd65671
Michael McEniry [UAH] <mcenirm@uah.edu>
2016-04-20 20:08:15 -0500

    Protect your view

--- mountain/urls.py
+++ mountain/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from .views import ApiEndpoint
+from .views import ApiEndpoint, secret_page
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='pages/home.html'), name='home'),
@@ -10,4 +10,5 @@ urlpatterns = [
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^api/hello', ApiEndpoint.as_view()),
+    url(r'^secret$', secret_page, name='secret'),
 ]
--- mountain/views.py
+++ mountain/views.py
@@ -1,7 +1,13 @@
-from oauth2_provider.views.generic import ProtectedResourceView
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from oauth2_provider.views.generic import ProtectedResourceView
 
 class ApiEndpoint(ProtectedResourceView):
 
     def get(self, request, *args, **kwargs):
         return HttpResponse('Hello, OAuth2!')
+
+
+@login_required()
+def secret_page(request, *args, **kwargs):
+    return HttpResponse('Secret contents!', status=200)
